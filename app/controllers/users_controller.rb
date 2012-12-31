9144d7256e222bfdf3f6441456f6e490a5fe447a
Kwadwo Adu <k@adu.dk>
2012-12-31 17:11:16 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -43,13 +44,6 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
-  end
-
-  def signed_in_user
-    unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-    end
   end
 
   private
